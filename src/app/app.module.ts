f0b31ff7cbc7bd52bf4bb22353a0bf3d74196fd7
gbemileke <gbemileke@MACs-MacBook-Air.local>
2019-12-25 15:30:05 +0100

    create camera page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { CameraPage } from '../pages/camera/camera';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ViewTextPage } from '../pages/view-text/view-text'
@@ -16,10 +16,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    CameraPage,
     HomePage,
     TabsPage,
-    ViewTextPage
+    ViewTextPage,
+
   ],
   imports: [
     BrowserModule,
@@ -29,7 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    CameraPage,
     HomePage,
     TabsPage,
     ViewTextPage
